22e2f16d108706e9354a3091bf5b4f4f30259fec
Andrew Holmes <acholmes001@gmail.com>
2022-03-23 23:00:14 -0400

    simple matrix multiplication working

--- strassen.py
+++ strassen.py
@@ -4,21 +4,39 @@ import sys
 def standard(file, dim):
     row = [0] * dim
     col = [0] * dim
+    offset = dim**2
     out = [[0 for x in range(dim)] for y in range(dim)] 
     with open(file) as input:
         lines = input.readlines()
-        for i in range(dim):
+        for r in range(dim):
+            for i in range(dim):
+                sum = 0
+                for k in range(dim):
+                    row[k] = int(lines[k + r*dim])
+                    col[k] = int(lines[k*dim + i + offset])
+                    sum += row[k] * col[k]
+                out[r][i] = sum
+    '''
+    for k in out:
+        for j in k:
+            print(j)
+    '''
+    return out
 
+''' Desired output:
+6 2 16
+18 15 27 
+9 9 18 '''
 
 
 def main():
-    if sys.argc != 4:
+    if (len(sys.argv) != 4):
         print("Usage: python3 strassen.py flag dimension inputfile")
         return 1
     flag = sys.argv[1]
     dim = sys.argv[2]
     file = sys.argv[3]
-    with open(file) as input:
+    standard(file, 3)
         
 
 if __name__ == "__main__":
